f26f78d2b9b784f52789f9b25546129ed4151235
heixiui <73188705+heixiui@users.noreply.github.com>
2023-07-17 19:14:16 +0800

    Add files via upload

---
+++ auto-momo/ip.py
@@ -0,0 +1,126 @@
+# encoding:utf-8
+import re
+from random import choice
+import asyncio
+from aiohttp import ClientSession, ClientTimeout, TCPConnector
+
+listIP = []  # 保存IP地址
+
+
+# 随机返回请求头
+async def getheaders():
+    headers_list = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"]
+    headers = {'User-Agent': choice(headers_list)}
+    return headers
+
+
+# 生成任务列表
+async def taskList(ss):
+    task = [
+        asyncio.create_task(get_page('http://www.kxdaili.com/dailiip/2/1.html', session=ss)),
+        asyncio.create_task(get_page('https://www.kuaidaili.com/free/inha/1/', mod=2, session=ss)),
+        asyncio.create_task(get_page('https://www.kuaidaili.com/free/intr/2/', mod=2, session=ss)),
+        asyncio.create_task(get_page('https://www.proxy-list.download/api/v1/get?type=http', mod=3, session=ss)),
+    ]
+
+    for i in range(1, 4):
+        task.append(asyncio.create_task(get_page(f'http://www.nimadaili.com/http/{i}/', mod=4, session=ss)))
+        task.append(asyncio.create_task(get_page(f'https://www.89ip.cn/index_{i}.html', session=ss)))
+        task.append(asyncio.create_task(get_page(f'http://http.taiyangruanjian.com/free/page{i}/', mod=1, session=ss)))
+        task.append(asyncio.create_task(get_page(f'http://www.kxdaili.com/dailiip/1/{i}.html', session=ss)))
+        task.append(asyncio.create_task(get_page(f'http://www.ip3366.net/free/?stype=1&page={i}', session=ss)))
+        task.append(asyncio.create_task(get_page(f'https://www.dieniao.com/FreeProxy/{i}.html', mod=5, session=ss)))
+    return task
+
+
+# 实例化请求对象
+async def create_aiohttp_ip():
+    async with ClientSession(connector=TCPConnector(ssl=False, limit=10)) as session:
+        task = await taskList(session)
+        await asyncio.wait(task)
+
+
+# 访问网页
+async def get_page(url, session, mod=0):
+    tout = ClientTimeout(total=20)
+    hd = await getheaders()
+    try:
+        async with asyncio.Semaphore(2):
+            async with await session.get(url=url, headers=hd, timeout=tout) as response:
+                page_source = await response.text()
+                await soup_page(page_source, mod=mod)
+    except Exception as e:
+        print(f"['{url}']抓取失败:", e)
+
+
+async def soup_page(source, mod):
+    if mod == 0:
+        # 通用
+        ips = re.findall(r'<td>[\s]*?(\d+\.\d+\.\d+\.\d+)[\s]*?</td>', source)
+        posts = re.findall(r'<td>[\s]*?(\d{1,5})[\s]*?</td>', source)
+        for i in range(len(ips)):
+            listIP.append(f"http://{ips[i]}:{posts[i]}")
+
+    elif mod == 1:
+        # 太阳
+        ips = re.findall(r'<div.*?">(\d+\.\d+\.\d+\.\d+)</div>', source)
+        posts = re.findall(r'<div.*?">(\d{1,5})</div>', source)
+        for i in range(len(ips)):
+            listIP.append(f"http://{ips[i]}:{posts[i]}")
+
+    elif mod == 2:
+        # 快代理
+        ips = re.findall(r'<td\s.*?="IP">(\d+\.\d+\.\d+\.\d+)</td>', source)
+        posts = re.findall(r'<td\s.*?="PORT">(\d{1,5})</td>', source)
+        for i in range(len(ips)):
+            listIP.append(f"http://{ips[i]}:{posts[i]}")
+
+    elif mod == 3:
+        # www.proxy-list.download/api/v1/get?type=http
+        ip_list = source.split('\r\n')[:-1]
+        for i in ip_list:
+            listIP.append(f"http://{i}")
+
+    elif mod == 4:
+        # 泥马代理
+        ip_post = re.findall(r'<td>(.*?:\d+)</td>', source)
+        for i in ip_post:
+            listIP.append(f"http://{i}")
+
+    elif mod == 5:
+        # 蝶鸟
+        ip_list = re.findall(r"<span\sclass='f-address'>(.*?)</span>", source)[1:]
+        port_list = re.findall(r"<span class='f-port'>(\d+)</span>", source)
+        for i in range(len(ip_list)):
+            listIP.append(f'http://{ip_list[i]}:{port_list[i]}')
+
+    elif mod == 6:
+        # 站大爷
+        pass
+
+
+def ip_main():
+    asyncio.run(create_aiohttp_ip())
+    global listIP
+    listIP = list(set(listIP))
+    print(f"代理ip抓取完成,共{len(listIP)}个可用代理ip地址。")
---
+++ auto-momo/momo.py
@@ -0,0 +1,55 @@
+# encoding:utf-8
+from os import environ
+from ip import listIP, getheaders, ip_main
+import asyncio
+from aiohttp import ClientSession, ClientTimeout
+
+global n  # 记录访问成功次数
+link = 'link'  # 设置link
+
+# 如果检测到程序在 github actions 内运行，那么读取环境变量中的登录信息
+if environ.get('GITHUB_RUN_ID', None):
+    link = environ['link']
+
+
+async def create_aiohttp(url, proxy_list):
+    global n
+    n = 0
+    async with ClientSession() as session:
+        # 生成任务列表
+        task = [asyncio.create_task(web_request(url=url, proxy=proxy, session=session)) for
+                proxy in proxy_list]
+        await asyncio.wait(task)
+
+
+# 网页访问
+async def web_request(url, proxy, session):
+    tout = ClientTimeout(total=20)
+    # 并发限制
+    async with asyncio.Semaphore(3):
+        try:
+            async with await session.get(url=url, headers=await getheaders(), proxy=proxy,
+                                         timeout=tout) as response:
+                # 返回字符串形式的相应数据
+                page_source = await response.text()
+                await page(page_source)
+        except Exception:
+            pass
+
+
+# 判断访问是否成功
+async def page(page_source):
+    global n
+    if "学习天数" in page_source:
+        n += 1
+
+
+def main():
+    ip_main()  # 抓取代理
+    proxies = [i.strip() for i in listIP]  # 生成代理列表
+    asyncio.run(create_aiohttp(link, proxies))
+    print(f"墨墨分享链接访问成功{n}次。")
+
+
+if __name__ == '__main__':
+    main()
